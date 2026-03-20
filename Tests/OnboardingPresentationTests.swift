950f18a3971206bf0bbfda6824749a7e5a2ce295
johnwatso <johnhendersonnet@icloud.com>
2026-03-21 10:54:29 +1300

    Ongoing Dev work for 1.0

---
+++ Tests/OnboardingPresentationTests.swift
@@ -0,0 +1,100 @@
+import XCTest
+@testable import SwiftTwitchMiner
+@testable import SwiftMiner
+
+@MainActor
+final class OnboardingPresentationTests: XCTestCase {
+
+    var navigation: NavigationModel!
+    var settings: Settings!
+    var minerManager: MinerManager!
+
+    override func setUp() {
+        super.setUp()
+        settings = Settings.shared
+        settings.resetToDefaults()
+        
+        minerManager = MinerManager(clientId: "test")
+        navigation = NavigationModel(clientId: "test", minerManager: minerManager)
+    }
+
+    // MARK: - Tests
+
+    func test_0Accounts_NotDismissed_PanelVisible() {
+        // Given
+        XCTAssertTrue(minerManager.miners.isEmpty)
+        XCTAssertFalse(settings.hasDismissedOnboarding)
+        
+        // When
+        navigation.refreshOnboardingPresentation()
+        
+        // Then
+        XCTAssertTrue(navigation.showOnboarding)
+        XCTAssertEqual(navigation.onboardingPresentation?.accountState, .noAccounts)
+        XCTAssertEqual(navigation.onboardingPresentation?.title, "Connect an account when you're ready")
+    }
+
+    func test_1PlusAccounts_NoPreferences_PanelVisible_GamePrompt() {
+        // Given
+        let account = Account(id: "1", username: "test", accessToken: "token", refreshToken: "refresh", tokenExpiry: Date().addingTimeInterval(3600), scopes: [])
+        minerManager.addAccount(account)
+        XCTAssertFalse(minerManager.miners.isEmpty)
+        XCTAssertTrue(settings.gamePreferences.isEmpty)
+        
+        // When
+        navigation.refreshOnboardingPresentation()
+        
+        // Then
+        XCTAssertTrue(navigation.showOnboarding)
+        XCTAssertTrue(navigation.onboardingPresentation?.showsGamePreferences ?? false)
+        XCTAssertEqual(navigation.onboardingPresentation?.title, "Refine what SwiftMiner should prioritize")
+    }
+
+    func test_1PlusAccounts_Preferences_NotSyncing_PanelHidden() {
+        // Given
+        let account = Account(id: "1", username: "test", accessToken: "token", refreshToken: "refresh", tokenExpiry: Date().addingTimeInterval(3600), scopes: [])
+        minerManager.addAccount(account)
+        
+        let game = Game(id: "g1", name: "Test Game")
+        settings.addGamePreference(game, state: .preferred)
+        
+        XCTAssertFalse(minerManager.miners.isEmpty)
+        XCTAssertFalse(settings.gamePreferences.isEmpty)
+        XCTAssertFalse(navigation.isRunningOnboardingSetup)
+        
+        // When
+        navigation.refreshOnboardingPresentation()
+        
+        // Then
+        XCTAssertFalse(navigation.showOnboarding)
+        XCTAssertNil(navigation.onboardingPresentation)
+    }
+
+    func test_Dismissed_PanelHidden() {
+        // Given
+        settings.hasDismissedOnboarding = true
+        
+        // When
+        navigation.refreshOnboardingPresentation()
+        
+        // Then
+        XCTAssertFalse(navigation.showOnboarding)
+        XCTAssertNil(navigation.onboardingPresentation)
+    }
+
+    func test_SyncInProgress_SetupStatusVisible() {
+        // Given
+        let account = Account(id: "1", username: "test", accessToken: "token", refreshToken: "refresh", tokenExpiry: Date().addingTimeInterval(3600), scopes: [])
+        minerManager.addAccount(account)
+        
+        // When
+        navigation.isRunningOnboardingSetup = true
+        navigation.updateOnboardingSetupStage(.campaigns)
+        
+        // Then
+        XCTAssertTrue(navigation.showOnboarding)
+        XCTAssertNotNil(navigation.onboardingPresentation?.setupStage)
+        XCTAssertEqual(navigation.onboardingPresentation?.setupStage, .campaigns)
+        XCTAssertEqual(navigation.onboardingPresentation?.title, "Syncing your account")
+    }
+}
